2013f376d0fe36dfa3499bd8fc9c1937947694ec
tetracup <alexsklbus@gmail.com>
2023-01-06 12:06:33 +0100

    completed five in a row with rudimentary ai

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
+
+
 let slider = document.querySelector("#resSlider")
 let container = document.querySelector(".container")
 var containerChildren 
 let textInput = document.querySelector("#textInput")
 let dimensions = document.querySelector("#dimensions")
 let restart = document.querySelector("#restart")
+let endText = document.querySelector("#endText")
 let isPressed = false
 let gameInProgress = true
 let areGridLinesOn
 var gameboard = [[]]
+let maxVal = 0
+let playerWon = true
+let previousPlayerMove
 const SlotStatus = {
     None: 0,
 	Player: 1,
@@ -15,6 +21,9 @@ const SlotStatus = {
 }
 function UpdateGrid()
 {
+    //Reinitialise values based on restart or dimension change
+    endText.textContent = ""
+    document.body.style.backgroundColor = "white"
     dimensions.textContent = `${slider.value}x${slider.value}`
     textInput.value = slider.value
     container.style.cssText = `grid-template-columns: repeat(${slider.value}, auto)`
@@ -40,45 +49,133 @@ function UpdateGrid()
     }
     containerChildren = Array.from(container.childNodes)
     gameInProgress = true
+    maxVal = parseInt(slider.value)
 }
 function PlaceMarker(evt)
 {
+    //Check that marker is empty and game is ongoing
     if(evt.target.getAttribute('status') != SlotStatus.None || !gameInProgress)
         return;
+    //Set Player values to slot
     evt.target.setAttribute('status', SlotStatus.Player)
     evt.target.style.backgroundColor = `rgba(0, 0, 255, 0.3)`
     containerChildren.splice(containerChildren.indexOf(evt.target), 1)
-    
+    previousPlayerMove = evt.target
 
+    //Check if placed marker was a win
     if(CheckWin(false, evt.target.dataset.x, evt.target.dataset.y))
     {
-        setTimeout(function() {
-            alert("You Win!");
-          }, 0)
+        playerWon = true
+        UpdateBackgroundOnEnd()
         gameInProgress = false
         return 
     }
-
+    //If No avaliable slots left then its a tie
     if(CheckTie())
         return
 
-
+    //Perform Computers Move 
     PlaceComputerMarker()
+
     
 }
 
 function PlaceComputerMarker()
 {
-    const randIndex = Math.floor(Math.random() * containerChildren.length)
-    containerChildren[randIndex].style.backgroundColor = `rgba(255, 0, 0, 0.3)`
-    containerChildren[randIndex].setAttribute('status', SlotStatus.Computer)
-    containerChildren.splice(randIndex, 1)
+    //Get new index and initialise slot
+    const newIndex = ChooseNextComputerMove()
+    containerChildren[newIndex].style.backgroundColor = `rgba(255, 0, 0, 0.3)`
+    containerChildren[newIndex].setAttribute('status', SlotStatus.Computer)
+    
+    //Check if its a win, if so end game
+    if(CheckWin(true, containerChildren[newIndex].dataset.x, containerChildren[newIndex].dataset.y))
+    {
+        playerWon = false
+        UpdateBackgroundOnEnd()
+        gameInProgress = false
+        return 
+    }
+    containerChildren.splice(newIndex, 1)
+    //Check if its a loss, if so end game
     if(CheckTie())
         return
+    
+}
+
+function ChooseNextComputerMove()
+{
+    //Computer moves based on players move, "defensive"
+    startX = parseInt(previousPlayerMove.dataset.x)
+    startY = parseInt(previousPlayerMove.dataset.y)
+    let increments = 
+    [
+        [0,1],
+        [1,1],
+        [1,0],
+        [1,-1],
+        [0,-1],
+        [-1, -1],
+        [-1, 0],
+        [-1, 1]
+    ]
+    let emptyCells = []
+    let newIndex = -1
+    //Check every slot surrounding previous players move
+    increments.every(function(element)
+    {
+        newX = startX + element[0]
+        newY = startY + element[1]
+        oppositeX = startX - element[0]
+        oppositeY = startY - element[1]
+        if(!CheckBounds(newX, newY))
+            return true
+        if(gameboard[newX][newY].getAttribute('status') == SlotStatus.None)
+        {
+            //Push empty cells to array
+            emptyCells.push(gameboard[newX][newY])
+            return true
+        }
+        else if(CheckBounds(oppositeX, oppositeY))
+        {
+            if(gameboard[newX][newY].getAttribute('status') == SlotStatus.Player)
+            {
+                //If there is another player slot next to previous players move place computer marker
+                //on opposite side
+                if(gameboard[oppositeX][oppositeY].getAttribute('status') == SlotStatus.None)
+                {
+                    containerChildren.every(function (element) {
+                        if(oppositeX == element.dataset.x && oppositeY == element.dataset.y)
+                        {
+                            newIndex = containerChildren.indexOf(element)
+                            return false
+                        }
+                        return true
+                    })
+                }
+            }
+            return true
+        }
+    })
+    
+    if(newIndex != -1)
+        return newIndex
+    if(emptyCells.length != 0)
+    {
+        //If no player slot next to previous player move place on random empty 
+        //slot near to previous player move
+        eIdx = Math.floor(Math.random() * emptyCells.length)
+        newX = emptyCells[eIdx].dataset.x
+        newY = emptyCells[eIdx].dataset.y
+        newIndex = containerChildren.indexOf(emptyCells[eIdx])
+        return newIndex
+    }
+    // Given that all slots are filled around players last move, place on random slot
+    return Math.floor(Math.random() * containerChildren.length)
 }
 
 function UpdateSlider()
 {
+    //If slider is changed update and restart grid
     if(textInput.value > 30)
         textInput.value = 30
     else if(textInput.value < 15)
@@ -90,6 +187,7 @@ function UpdateSlider()
 
 function CheckTie()
 {
+    //If no more empty slots end game in tie
     if(containerChildren.length <= 0)
     {
         setTimeout(function() {
@@ -103,44 +201,87 @@ function CheckTie()
 
 function CheckWin(isComputer, x, y)
 {
-    let counter = 1
     var checkState
     isComputer ? checkState = SlotStatus.Computer : checkState = SlotStatus.Player
-    var incY = 1
-    let originalX = x
-    let originalY = y
-    while(gameboard[x][++y].getAttribute('status') == checkState)
-    {
-        counter++
-    }
-    y = originalY
-    while(gameboard[x][--y].getAttribute('status') == checkState)
-    {
-        counter++
-    }
-    console.log(counter)
-    if(counter == 5)
-            return true
-    return false
-    /*
-    for(i = -1; i <= 1; i++)
-    {
-        for(a = -1; a <= 1; a++)
+    x = parseInt(x)
+    y = parseInt(y)
+    const increments = 
+    [
+        [0,1],
+        [1,1],
+        [1,0],
+        [1,-1]
+    ]
+    counterEnough = false
+    //Cycle through each increment checking how many slots computer/player has in a line, 
+    //each increment has a counter which when over 5 ends game
+    increments.every(element => 
         {
-            if(i == 0 && a == 0)
-                continue
-            if(gameboard[x + i][y + a].getAttribute('status') == checkState)
+            var counter = 1
+            var incX = 0
+            var incY = 0
+            incX = x+parseInt(element[0])
+            incY = y+parseInt(element[1])
+            if(CheckBounds(incX,incY))
             {
-                
+                while(gameboard[incX][incY].getAttribute('status') == checkState)
+                {
+                    counter++
+                    incX = incX+parseInt(element[0])
+                    incY = incY+parseInt(element[1])
+                    if(!CheckBounds(incX,incY))
+                        break;
+                }
             }
-        }
-    }
-    */
+            incX = x-parseInt(element[0])
+            incY = y-parseInt(element[1])
+            if(CheckBounds(incX,incY))
+            {
+                while(gameboard[incX][incY].getAttribute('status') == checkState)
+                {
+                    counter++
+                    incX = incX-parseInt(element[0])
+                    incY = incY-parseInt(element[1])
+                    if(!CheckBounds(incX,incY))
+                        break;
+                }
+            }
+            if(counter >= 5)
+            {
+                counterEnough = true
+                return false
+            }
+            return true
+                
+        })
+        return counterEnough
 }
 
+function CheckBounds(x, y)
+{
+    //Only perform actions if x and y in bounds
+    return x >= 0 && y >= 0 && x < maxVal && y < maxVal
+}
+
+function UpdateBackgroundOnEnd()
+{
+    //Update background on game end
+    if(playerWon)
+    {
+        endText.textContent = "You Win!"
+        endText.style.color = "blue"
+        document.body.style.backgroundColor = "rgb(93, 93, 255)"
+    }
+    else
+    {
+        endText.textContent = "Computer Won!"
+        endText.style.color = "red"
+        document.body.style.backgroundColor = "rgb(255, 93, 93)"
+    }
+}
+
+
 UpdateGrid();
-
-
 slider.onchange = UpdateGrid;
 textInput.onchange = UpdateSlider
 restart.onclick = UpdateGrid
